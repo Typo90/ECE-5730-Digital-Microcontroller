4c11e1bdab30823ef5852615ba8e5379dc311c3b
Typo90 <yp284@cornell.edu>
2022-09-28 13:24:51 -0400

    Update animation.c (undone)

--- Lab_2/Animation_Demo/animation.c
+++ Lab_2/Animation_Demo/animation.c
@@ -75,32 +75,36 @@ fix15 boid1_y ;
 fix15 boid1_vx ;
 fix15 boid1_vy ;
 
+
+// number of boids
+int n = 15;
 //
-fix15 boid_x[10];
-fix15 boid_y[10];
-fix15 boid_vx[10];
-fix15 boid_vy[10];
+fix15 boid_x[n];
+fix15 boid_y[n];
+//
+fix15 boid_vx[n];
+fix15 boid_vy[n];
 
 //
-int id[10];
+int id[n];
 //count boid
 volatile unsigned int boid_count = 0;
 //=======================================
 // Boids algorithm varibale
 //=======================================
 fix15 turnfactor = float2fix15(0.2);
-fix15 visualRange = 40;
-fix15 protectedRange = 8;
-fix15 centering_factor = 0.0005;
-fix15 avoidfactor = 0.05;
-fix15 matching_factor = 0.05;
+fix15 visualRange = int2fix15(40);
+fix15 protectedRange = int2fix15(8);
+fix15 centering_factor = float2fix15(0.0005);
+fix15 avoidfactor = float2fix15(0.05);
+fix15 matching_factor = float2fix15(0.05);
 fix15 maxspeed = int2fix15(6);
 fix15 minspeed = int2fix15(3);
-fix15 maxbias = 0.01;
-fix15 bias_increment = 0.00004;
+fix15 maxbias = float2fix15(0.01);
+fix15 bias_increment = float2fix15(0.00004);
 //(user-changeable, or updated dynamically)
-fix15 biasval_0 = 0.001; 
-fix15 biasval_1 = 0.001; 
+fix15 biasval_0 = float2fix15(0.001); 
+fix15 biasval_1 = float2fix15(0.001); 
 
 //=======================================
 
@@ -141,182 +145,177 @@ void drawArena() {
 void wallsAndEdges(fix15* x, fix15* y, fix15* vx, fix15* vy)
 {
 
-  
+    
 
   // //=======================================
   // // Boids algorithm 
   // //=======================================
 
-  // fix15 xpos_avg = 0;
-  // fix15 ypos_avg = 0;
-  // fix15 xvel_avg = 0;
-  // fix15 yvel_avg = 0;
-
-  // int neighboring_boids = 0;
-  // fix15 close_dx = 0;
-  // fix15 close_dy = 0;
-
-  // fix15 dx = boid0_x - boid1_x;
-  // fix15 dy = boid0_y - boid1_y;
-
-  // if(abs(dx) < visualRange && abs(dy) < visualRange){
-  //   fix15 squared_distance = dx*dx + dy*dy;
-
-  //   if(squared_distance < protectedRange){
-  //     close_dx += boid0_x - boid1_x;
-  //     close_dy += boid0_y - boid1_y;
-  //   }else if(squared_distance < visualRange){
-  //     xpos_avg += boid1_x;
-  //     ypos_avg += boid1_y;
-  //     xvel_avg += boid1_vx;
-  //     yvel_avg += boid1_vy;
-
-  //     neighboring_boids += 1;
-  //   } 
-  // }
-
-  // if(neighboring_boids > 0){
-  //   //Divide accumulator variables by number of boids in visual range
-  //   xpos_avg = xpos_avg/neighboring_boids;
-  //   ypos_avg = ypos_avg/neighboring_boids;
-  //   xvel_avg = xvel_avg/neighboring_boids;
-  //   yvel_avg = yvel_avg/neighboring_boids;
-
-  //   //Add the centering/matching contributions to velocity
-  //   boid0_vx = (boid0_vx + 
-  //               (xpos_avg - boid0_x)*centering_factor + 
-  //               (xvel_avg - boid0_vx)*matching_factor);
-
-  //   boid0_vy = (boid0_vy + 
-  //               (ypos_avg - boid0_y)*centering_factor + 
-  //               (yvel_avg - boid0_vy)*matching_factor);
-  // }
-  // // Add the avoidance contribution to velocity
-  // boid0_vx = boid0_vx + (close_dx*avoidfactor);
-  // boid0_vy = boid0_vy + (close_dy*avoidfactor);
-      
-  // // If the boid is near an edge, make it turn by turnfactor
-  // //(this describes a box, will vary based on boundary conditions)
-  // if (hitTop(*y)){
-  //     boid0_vy = boid0_vy + turnfactor;
-  // }
-  // if (hitRight(*x)){
-  //     boid0_vx = boid0_vx - turnfactor;
-  // }
-  // if (hitLeft(*x)){
-  //     boid0_vx = boid0_vx + turnfactor;
-  // }
-  // if (hitBottom(*y)){
-  //     boid0_vy = boid0_vy - turnfactor;
-  // }
-
-  // //##############################################################
-  // //### ECE 5730 students only - dynamically update bias value ###
-  // //##############################################################
-  // //# biased to right of screen
-  // if(boid0_vx > 0){
-  //   biasval_0 = min(maxbias, biasval_0 + bias_increment);
-  // }else{
-  //   biasval_0 = max(bias_increment, biasval_0 - bias_increment);
-  // }
-
-  // if(boid1_x < 0){
-  //   biasval_1 = min(maxbias, biasval_1 + bias_increment);
-  // }else{
-  //   biasval_1 = max(bias_increment, biasval_1 - bias_increment);
-  // }
-
-  // //##############################################################
-  // //# If the boid has a bias, bias it!
-  // //# biased to right of screen
-
-  //   boid0_vx = (1 - biasval_0)*boid0_vx + (biasval_0 * 1);
-  // //# biased to left of screen
-  //   boid1_vx = (1 - biasval_1)*boid1_vx + (biasval_1 * (-1));
-
-  // //# Calculate the boid's speed
-  // //# Slow step! Lookup the "alpha max plus beta min" algorithm
-  // fix15 speed = sqrt(multfix15(*vx, *vx) + multfix15(*vy, *vy));
-
-  // //# Enforce min and max speeds
-  // if(speed < minspeed){
-  //     *vx = (*vx/speed)*minspeed;
-  //     *vy = (*vy/speed)*minspeed;
-  // }
-  // if(speed > maxspeed){
-  //     *vx = (*vx/speed)*maxspeed;
-  //     *vy = (*vy/speed)*maxspeed;
-  // }
-
-  // //# Update boid's position
-  // *x = *x + *vx;
-  // *y = *y + *vy;
-  // // boid.x = boid.x + boid.vx
-  // // boid.y = boid.y + boid.vy
-
+  int i = 0;
+  int j = 0;
+  for(i = 0; i<n; i++){
     
-  //   // if (boid in scout group 1): 
-  //   //     if (boid.vx > 0):
-  //   //         boid.biasval = min(maxbias, boid.biasval + bias_increment)
-  //   //     else:
-  //   //         boid.biasval = max(bias_increment, boid.biasval - bias_increment)
-  //   //# biased to left of screen
-  //   // else if (boid in scout group 2): # biased to left of screen
-  //   //     if (boid.vx < 0):
-  //   //         boid.biasval = min(maxbias, boid.biasval + bias_increment)
-  //   //     else:
-  //   //         boid.biasval = max(bias_increment, boid.biasval - bias_increment)
-  //   //##############################################################
-  
-  
+    fix15 xpos_avg = 0;
+    fix15 ypos_avg = 0;
+    fix15 xvel_avg = 0;
+    fix15 yvel_avg = 0;
 
-  // Reverse direction if we've hit a wall
-  if (hitTop(*y)) {
-    *vy = *vy + turnfactor ;
+    int neighboring_boids = 0;
+    fix15 close_dx = 0;
+    fix15 close_dy = 0;
+
+    for(j = 0; j<n; j++){
+      if(j == i){
+        continue;
+      }
+      fix15 dx = boid_x[i] - boid_x[j];
+      fix15 dy = boid_y[i] - boid_y[j];
+      
+      if(abs(dx) < visualRange && abs(dy) < visualRange){
+        fix15 squared_distance = multfix15(dx, dx) + multfix15(dy, dy);
+
+        if(squared_distance < protectedRange){
+          close_dx += boid_x[i] - boid_x[j];
+          close_dy += boid_y[i] - boid_y[j];
+        }else if(squared_distance < visualRange){
+          xpos_avg += boid_x[j];
+          ypos_avg += boid_y[j];
+          xvel_avg += boid_vx[j];
+          yvel_avg += boid_vy[j];
+
+          neighboring_boids += 1;
+        } 
+      }
+    }
+    if(neighboring_boids > 0){
+      //Divide accumulator variables by number of boids in visual range
+      xpos_avg = xpos_avg/neighboring_boids;
+      ypos_avg = ypos_avg/neighboring_boids;
+      xvel_avg = xvel_avg/neighboring_boids;
+      yvel_avg = yvel_avg/neighboring_boids;
+
+      //Add the centering/matching contributions to velocity
+      boid_vx[i] = (boid_vx[i] + 
+                  (xpos_avg - boid_x[i])*centering_factor + 
+                  (xvel_avg - boid_vx[i])*matching_factor);
+
+      boid_vy[i] = (boid_vy[i] + 
+                  (ypos_avg - boid_y[i])*centering_factor + 
+                  (yvel_avg - boid_vy[i])*matching_factor);
+    }
+    // Add the avoidance contribution to velocity
+    boid_vx[i] = boid_vx[i] + (close_dx*avoidfactor);
+    boid_vy[i] = boid_vy[i] + (close_dy*avoidfactor);
+        
+    // If the boid is near an edge, make it turn by turnfactor
+    //(this describes a box, will vary based on boundary conditions)
+    if (hitTop(*y)){
+        boid_vy[i] = boid_vy[i] + turnfactor;
+    }
+    if (hitRight(*x)){
+        boid_vx[i] = boid_vx[i] - turnfactor;
+    }
+    if (hitLeft(*x)){
+        boid_vx[i] = boid_vx[i] + turnfactor;
+    }
+    if (hitBottom(*y)){
+        boid_vy[i] = boid_vy[i] - turnfactor;
+    }
+
+    //##############################################################
+    //### ECE 5730 students only - dynamically update bias value ###
+    //##############################################################
+    //# biased to right of screen
+    if(boid_vx[i] > 0){
+      biasval_0 = min(maxbias, biasval_0 + bias_increment);
+    }else{
+      biasval_0 = max(bias_increment, biasval_0 - bias_increment);
+    }
+
+    if(boid_x[j] < 0){
+      biasval_1 = min(maxbias, biasval_1 + bias_increment);
+    }else{
+      biasval_1 = max(bias_increment, biasval_1 - bias_increment);
+    }
+
+    //##############################################################
+    //# If the boid has a bias, bias it!
+    //# biased to right of screen
+
+      boid_vx[i] = (1 - biasval_0)*boid_vx[i] + (biasval_0 * 1);
+    //# biased to left of screen
+      boid_vx[j] = (1 - biasval_1)*boid_vx[j] + (biasval_1 * (-1));
+
+    //# Calculate the boid's speed
+    //# Slow step! Lookup the "alpha max plus beta min" algorithm
+    fix15 speed = sqrt(multfix15(*vx, *vx) + multfix15(*vy, *vy));
+
+    //# Enforce min and max speeds
+    if(speed < minspeed){
+        *vx = (*vx/speed)*minspeed;
+        *vy = (*vy/speed)*minspeed;
+    }
+    if(speed > maxspeed){
+        *vx = (*vx/speed)*maxspeed;
+        *vy = (*vy/speed)*maxspeed;
+    }
+
+    //# Update boid's position
+    *x = *x + *vx;
+    *y = *y + *vy;
+    // boid.x = boid.x + boid.vx
+    // boid.y = boid.y + boid.vy
+
+      
+      // if (boid in scout group 1): 
+      //     if (boid.vx > 0):
+      //         boid.biasval = min(maxbias, boid.biasval + bias_increment)
+      //     else:
+      //         boid.biasval = max(bias_increment, boid.biasval - bias_increment)
+      //# biased to left of screen
+      // else if (boid in scout group 2): # biased to left of screen
+      //     if (boid.vx < 0):
+      //         boid.biasval = min(maxbias, boid.biasval + bias_increment)
+      //     else:
+      //         boid.biasval = max(bias_increment, boid.biasval - bias_increment)
+      //##############################################################
+    
+    
+
+    // Reverse direction if we've hit a wall
+    if (hitTop(*y)) {
+      *vy = *vy + turnfactor ;
+    }
+    if (hitBottom(*y)) {
+      *vy = *vy - turnfactor ;
+    } 
+    if (hitRight(*x)) {
+      *vx = *vx - turnfactor;
+    }
+    if (hitLeft(*x)) {
+      *vx = *vx + turnfactor;
+    } 
+
+    float temp1 = fix2float15(*vx);
+    float temp2 = fix2float15(*vy);
+    fix15 speed =  float2fix15(sqrt(temp1*temp1 + temp2 * temp2));
+
+
+    if(speed < minspeed){
+        *vx = multfix15(divfix(*vx,speed), minspeed);
+        *vy = multfix15(divfix(*vy,speed), minspeed);
+    }
+    if(speed > maxspeed){
+        *vx = multfix15(divfix(*vx, speed), maxspeed);
+        *vy = multfix15(divfix(*vy, speed), maxspeed);
+    }
+
+    //printf("vx: %d, vy: %d \n", fix2int15(*vx),fix2int15(*vy));
+
+    //Update position using velocity
+    *x = *x + *vx ;
+    *y = *y + *vy ;
   }
-  if (hitBottom(*y)) {
-    *vy = *vy - turnfactor ;
-  } 
-  if (hitRight(*x)) {
-    *vx = *vx - turnfactor;
-  }
-  if (hitLeft(*x)) {
-    *vx = *vx + turnfactor;
-  } 
-  // if(*y >= 380){
-  //   *vy = *vy - turnfactor ;
-  // }
-  // if(*y <= 100){
-  //   *vy = *vy + turnfactor ;
-  // }
-
-  // if(*x >= 540){
-  //   *vx = *vx - turnfactor;
-  // }
-
-  // if(*x <= 100){
-  //   *vx = *vx + turnfactor;
-  // }
-
-  float temp1 = fix2float15(*vx);
-  float temp2 = fix2float15(*vy);
-  fix15 speed =  float2fix15(sqrt(temp1*temp1 + temp2 * temp2));
-
-
-  if(speed < minspeed){
-      *vx = multfix15(divfix(*vx,speed), minspeed);
-      *vy = multfix15(divfix(*vy,speed), minspeed);
-  }
-  if(speed > maxspeed){
-      *vx = multfix15(divfix(*vx, speed), maxspeed);
-      *vy = multfix15(divfix(*vy, speed), maxspeed);
-  }
-
-  //printf("vx: %d, vy: %d \n", fix2int15(*vx),fix2int15(*vy));
-
-  //Update position using velocity
-  *x = *x + *vx ;
-  *y = *y + *vy ;
 }
 
 // ==================================================
