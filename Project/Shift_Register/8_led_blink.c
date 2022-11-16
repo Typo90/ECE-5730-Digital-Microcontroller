6664c3c8e0e326507691ef016e6b4163868d52c8
Typo90 <yp284@cornell.edu>
2022-11-16 11:29:41 -0500

    Successfully blink 8 led

--- Project/Shift_Register/8_led_blink.c
+++ Project/Shift_Register/8_led_blink.c
@@ -31,35 +31,71 @@ GND |   A   |   QH'
 74HC595     pico
 -------     ----
 VCC         3.3V
-SER         GPIO 9
+SER         GPIO 12
 OE          GND
 RCLK        GPIO 10
 SRCLK       GPIO 11
-CRCLR       3.3V
+SRCLR       3.3V
 */
 
-void main()
-{
-  ShiftRegister reg = shift_register_new((PinConfig){
-      .SERIAL_PIN = 9,
-      .SHIFT_REGISTER_CLOCK_PIN = 11,
-      .STORAGE_REGISTER_CLOCK_PIN = 10});
+// void main()
+// {
+//   ShiftRegister reg = shift_register_new((PinConfig){
+//       .SERIAL_PIN = 9,
+//       .SHIFT_REGISTER_CLOCK_PIN = 11,
+//       .STORAGE_REGISTER_CLOCK_PIN = 10});
 
-  int switch_on = 0;
+//   int switch_on = 0;
 
-  while (true)
-  {
-    shift_register_write_bit(&reg, 1);
-    shift_register_flush(&reg);
+//   while (true)
+//   {
+//     shift_register_write_bit(&reg, 1);
+//     shift_register_flush(&reg);
 
-    sleep_ms(1050);
-    switch_on++;
+//     sleep_ms(1050);
+//     switch_on++;
 
-    if (switch_on = 8)
-    {
-      shift_register_reset_storage(&reg);
-      sleep_ms(1050);
-      switch_on = 0;
+//     if (switch_on = 8)
+//     {
+//       shift_register_reset_storage(&reg);
+//       sleep_ms(1050);
+//       switch_on = 0;
+//     }
+//   }
+// }
+
+#include "shift_register_74hc595.h"
+
+// Define shift register pins
+#define ROW_1 12
+#define COl 9
+#define CLK_PIN 11
+#define LATCH_PIN 10
+
+int main() {
+    // Set clk pin as output
+    gpio_init(CLK_PIN);
+    gpio_set_dir(CLK_PIN, GPIO_OUT);
+    
+    // Set data pin as output
+    gpio_init(ROW_1);
+    gpio_set_dir(ROW_1, GPIO_OUT);
+
+    // Set latch pin as output
+    gpio_init(LATCH_PIN);
+    gpio_set_dir(LATCH_PIN, GPIO_OUT);
+
+    // Create new shift register
+    struct shift_register_74hc595_t *myreg = new_shreg_74hc595(CLK_PIN, ROW_1, LATCH_PIN);
+    
+    while (1) {
+        for (size_t qi = QA; qi <= QH; qi++) {
+            shreg_74hc595_put(myreg, qi, 1);
+            sleep_ms(500);
+        }
+        for (size_t qi = QA; qi <= QH; qi++) {
+            shreg_74hc595_put(myreg, qi, 0);
+            sleep_ms(500);
+        }
     }
-  }
 }
